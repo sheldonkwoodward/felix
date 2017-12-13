0f2601b162a516e756d35b55b3508720234d619f
sheldonwoodward <sheldon.k.woodward@gmail.com>
2017-12-12 21:46:05 -0800

    Added querying functionality for query_movie. Returns JSON object.

--- media/urls.py
+++ media/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('query/movie', views.query_movie, name='query_movie'),
-    path('query/season', views.query_season, name='query_season')
+    path('movie', views.query_movie, name='query_movie'),
+    path('season', views.query_season, name='query_season')
 ]
--- media/views.py
+++ media/views.py
@@ -9,8 +9,30 @@ def index(request):
 
 def query_movie(request):
     all_movies = Movie.objects.all()
-    data = {"results": []}
+    data = {
+        "status": [],
+        "results": []
+    }
+
     for movie in all_movies:
+        # TODO: add status messages for bad queries
+        # TODO: use for loop instead of if statements
+        if request.GET.get('id') is not None and str(movie.id) != request.GET.get('id'):
+            continue
+        elif request.GET.get('title') is not None and movie.title not in request.GET.get('title'):
+            continue
+        elif request.GET.get('release_year') is not None and str(movie.release_year) != request.GET.get('release_year'):
+            continue
+        elif request.GET.get('cut') is not None and movie.cut not in request.GET.get('cut'):
+            continue
+        elif request.GET.get('resolution') is not None and movie.resolution not in request.GET.get('resolution'):
+            continue
+        # TODO: implement date_added search, doesn't work with normal method
+        elif request.GET.get('length_minutes') is not None and str(movie.length_minutes) != request.GET.get(
+                'length_minutes'):
+            continue
+        # TODO: implement path search, doesnt work with normal method
+
         single_json = {
             'id': movie.id,
             'title': movie.title,
@@ -25,18 +47,9 @@ def query_movie(request):
     return JsonResponse(data)
 
 
+# TODO: implement query_season
 def query_season(request):
-    all_seasons = Season.objects.all()
-    data = {"results": []}
-    for season in all_seasons:
-        single_json = {
-            'id': season.id,
-            'title': season.title,
-            'season': season.season,
-            'cut': season.cut,
-            'resolution': season.resolution,
-            'date_added': season.date_added,
-            'path': season.path,
-        }
-        data['results'].append(single_json)
-    return JsonResponse(data)
+    return JsonResponse({
+        "status": [],
+        "results": []
+    })
