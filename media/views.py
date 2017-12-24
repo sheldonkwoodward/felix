ebcac4dcd7cec6630884430a1f53d7961359c400
sheldonwoodward <sheldon.k.woodward@gmail.com>
2017-12-24 13:01:09 -0900

    Final cleanup.

--- media/views.py
+++ media/views.py
@@ -5,7 +5,7 @@ from media.models import Movie, Season
 from datetime import datetime, date, timedelta
 
 
-def search_movie(movie_list, season_list, request):
+def search_media(movie_list, season_list, request):
     data = {
         "time_stamp": "2000-01-01 00:00:00.000000",
         "movie_num": 0,
@@ -81,50 +81,50 @@ def media_all(request):
     print('GET ALL MEDIA')
     movies = Movie.objects.all()
     seasons = Season.objects.all()
-    return JsonResponse(search_movie(movies, seasons, request))
+    return JsonResponse(search_media(movies, seasons, request))
 
 
 def media_date_day(request, year, month, day):
     movies = Movie.objects.filter(date_added__year=year).filter(date_added__month=month).filter(date_added__day=day)
     seasons = Season.objects.filter(date_added__year=year).filter(date_added__month=month).filter(date_added__day=day)
-    return JsonResponse(search_movie(movies, seasons, request))
+    return JsonResponse(search_media(movies, seasons, request))
 
 
 def media_date_month(request, year, month):
     movies = Movie.objects.filter(date_added__year=year).filter(date_added__month=month)
     seasons = Season.objects.filter(date_added__year=year).filter(date_added__month=month)
-    return JsonResponse(search_movie(movies, seasons, request))
+    return JsonResponse(search_media(movies, seasons, request))
 
 
 def media_date_year(request, year):
     movies = Movie.objects.filter(date_added__year=year)
     seasons = Season.objects.filter(date_added__year=year)
-    return JsonResponse(search_movie(movies, seasons, request))
+    return JsonResponse(search_media(movies, seasons, request))
 
 
-def media_past_days(request, days=1):
+def media_past_days(request, days):
     oldest_date = date.today() - timedelta(days=days)
     movies = Movie.objects.filter(date_added__gte=oldest_date)
     seasons = Season.objects.filter(date_added__gte=oldest_date)
-    return JsonResponse(search_movie(movies, seasons, request))
+    return JsonResponse(search_media(movies, seasons, request))
 
 
-def media_past_weeks(request, weeks=1):
+def media_past_weeks(request, weeks):
     oldest_date = date.today() - timedelta(days=weeks * 7)
     movies = Movie.objects.filter(date_added__gte=oldest_date)
     seasons = Season.objects.filter(date_added__gte=oldest_date)
-    return JsonResponse(search_movie(movies, seasons, request))
+    return JsonResponse(search_media(movies, seasons, request))
 
 
-def media_past_months(request, months=1):
+def media_past_months(request, months):
     oldest_date = date.today() - timedelta(days=months * 31)
     movies = Movie.objects.filter(date_added__gte=oldest_date)
     seasons = Season.objects.filter(date_added__gte=oldest_date)
-    return JsonResponse(search_movie(movies, seasons, request))
+    return JsonResponse(search_media(movies, seasons, request))
 
 
-def media_past_years(request, years=1):
+def media_past_years(request, years):
     oldest_date = date.today() - timedelta(days=years * 365)
     movies = Movie.objects.filter(date_added__gte=oldest_date)
     seasons = Season.objects.filter(date_added__gte=oldest_date)
-    return JsonResponse(search_movie(movies, seasons, request))
+    return JsonResponse(search_media(movies, seasons, request))
