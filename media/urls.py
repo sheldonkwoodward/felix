e4ece13515f25a2be61b8cd078fb4f51f94b687a
sheldonwoodward <sheldon.k.woodward@gmail.com>
2017-12-24 12:36:08 -0900

    Added more querying urls, and merged movie and season queries into one.

--- media/urls.py
+++ media/urls.py
@@ -3,9 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('movie', views.movie_all, name='movie_all'),
-    path('movie/days/<int:days>', views.movie_days, name='movie_days'),
-    path('movie/weeks/<int:weeks>', views.movie_weeks, name='movie_weeks'),
-    path('movie/months/<int:months>', views.movie_months, name='movie_months'),
-    path('movie/years/<int:years>', views.movie_years, name='movie_years'),
+    path('', views.media_all, name='media_all'),
+    path('<int:year>/<int:month>/<int:day>/', views.media_date_day, name='media_date_day'),
+    path('<int:year>/<int:month>/', views.media_date_month, name='media_date_month'),
+    path('<int:year>/', views.media_date_year, name='media_date_year'),
+    path('days/<int:days>/', views.media_past_days, name='media_past_days'),
+    path('weeks/<int:weeks>/', views.media_past_weeks, name='media_past_weeks'),
+    path('months/<int:months>/', views.media_past_months, name='media_past_months'),
+    path('years/<int:years>/', views.media_past_years, name='media_past_years'),
 ]
--- media/views.py
+++ media/views.py
@@ -5,16 +5,17 @@ from media.models import Movie, Season
 from datetime import datetime, date, timedelta
 
 
-def search_movie(movie_list, request):
+def search_movie(movie_list, season_list, request):
     data = {
         "time_stamp": "2000-01-01 00:00:00.000000",
-        "num": 0,
-        "movies": []
+        "movie_num": 0,
+        "season_num": 0,
+        "movies": [],
+        "seasons": []
     }
 
+    # find movies
     for movie in movie_list:
-        # TODO: add status messages for bad queries
-        # TODO: use for loop instead of if statements
         if request.GET.get('id') is not None and str(movie.id) != request.GET.get('id'):
             continue
         elif request.GET.get('title') is not None and movie.title not in request.GET.get('title'):
@@ -25,11 +26,9 @@ def search_movie(movie_list, request):
             continue
         elif request.GET.get('resolution') is not None and movie.resolution not in request.GET.get('resolution'):
             continue
-        # TODO: implement date_added search, doesn't work with normal method
         elif request.GET.get('length_minutes') is not None and str(movie.length_minutes) != request.GET.get(
                 'length_minutes'):
             continue
-        # TODO: implement path search, doesnt work with normal method
 
         single_json = {
             'id': movie.id,
@@ -43,36 +42,85 @@ def search_movie(movie_list, request):
         }
         data['movies'].append(single_json)
 
+    # find seasons
+    for season in season_list:
+        if request.GET.get('id') is not None and str(movie.id) != request.GET.get('id'):
+            continue
+        elif request.GET.get('title') is not None and movie.title not in request.GET.get('title'):
+            continue
+        elif request.GET.get('season') is not None and str(movie.release_year) != request.GET.get('season'):
+            continue
+        elif request.GET.get('cut') is not None and movie.cut not in request.GET.get('cut'):
+            continue
+        elif request.GET.get('resolution') is not None and movie.resolution not in request.GET.get('resolution'):
+            continue
+
+        single_json = {
+            'id': season.id,
+            'title': season.title,
+            'season': season.season,
+            'cut': season.cut,
+            'resolution': season.resolution,
+            'date_added': season.date_added,
+            'path': season.path,
+        }
+        data['seasons'].append(single_json)
+
     # results info
     data['time_stamp'] = str(datetime.now())
-    data['num'] = len(data['movies'])
+    data['movie_num'] = len(data['movies'])
+    data['season_num'] = len(data['seasons'])
     return data
 
 
-def movie_all(request):
+def media_all(request):
+    print('GET ALL MEDIA')
     movies = Movie.objects.all()
-    return JsonResponse(search_movie(movies, request))
+    seasons = Season.objects.all()
+    return JsonResponse(search_movie(movies, seasons, request))
 
 
-def movie_days(request, days):
+def media_date_day(request, year, month, day):
+    movies = Movie.objects.filter(date_added__year=year).filter(date_added__month=month).filter(date_added__day=day)
+    seasons = Season.objects.filter(date_added__year=year).filter(date_added__month=month).filter(date_added__day=day)
+    return JsonResponse(search_movie(movies, seasons, request))
+
+
+def media_date_month(request, year, month):
+    movies = Movie.objects.filter(date_added__year=year).filter(date_added__month=month)
+    seasons = Season.objects.filter(date_added__year=year).filter(date_added__month=month)
+    return JsonResponse(search_movie(movies, seasons, request))
+
+
+def media_date_year(request, year):
+    movies = Movie.objects.filter(date_added__year=year)
+    seasons = Season.objects.filter(date_added__year=year)
+    return JsonResponse(search_movie(movies, seasons, request))
+
+
+def media_past_days(request, days=1):
     oldest_date = date.today() - timedelta(days=days)
     movies = Movie.objects.filter(date_added__gte=oldest_date)
-    return JsonResponse(search_movie(movies, request))
+    seasons = Season.objects.filter(date_added__gte=oldest_date)
+    return JsonResponse(search_movie(movies, seasons, request))
 
 
-def movie_weeks(request, weeks):
+def media_past_weeks(request, weeks=1):
     oldest_date = date.today() - timedelta(days=weeks * 7)
     movies = Movie.objects.filter(date_added__gte=oldest_date)
-    return JsonResponse(search_movie(movies, request))
+    seasons = Season.objects.filter(date_added__gte=oldest_date)
+    return JsonResponse(search_movie(movies, seasons, request))
 
 
-def movie_months(request, months):
+def media_past_months(request, months=1):
     oldest_date = date.today() - timedelta(days=months * 31)
     movies = Movie.objects.filter(date_added__gte=oldest_date)
-    return JsonResponse(search_movie(movies, request))
+    seasons = Season.objects.filter(date_added__gte=oldest_date)
+    return JsonResponse(search_movie(movies, seasons, request))
 
 
-def movie_years(request, years):
+def media_past_years(request, years=1):
     oldest_date = date.today() - timedelta(days=years * 365)
     movies = Movie.objects.filter(date_added__gte=oldest_date)
-    return JsonResponse(search_movie(movies, request))
+    seasons = Season.objects.filter(date_added__gte=oldest_date)
+    return JsonResponse(search_movie(movies, seasons, request))
